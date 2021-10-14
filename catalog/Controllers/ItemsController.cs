76e8c82943c10a6ec3f473ae87d77197c2d71b6b
AhmadAlz7 <amalzoubi92@gmail.com>
2021-10-14 06:55:36 +0300

    DI and DTO
    Implemented the Dependency Injection for GET and added the Data Transfer Object functionality

--- catalog/Controllers/ItemsController.cs
+++ catalog/Controllers/ItemsController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Catalog.DTOs;
 using Catalog.Entities;
 using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -11,27 +13,29 @@ namespace Catalog.Controllers
     [Route("[controller]")] //GET => ./items // you can also hard-type it "[items]"
     public class ItemsController : ControllerBase
     {
-        private readonly InMemItemsRepository repository;
+        private readonly IItemsRepository repository;
 
-        public ItemsController()
+        public ItemsController(IItemsRepository repository)
         {
-            repository = new InMemItemsRepository();
+            this.repository = repository;
         }
 
         [HttpGet] //GET ./items
-        public IEnumerable<Item> GetItems()
+        public IEnumerable<ItemDto> GetItems()
         {
-            var items = repository.GetItems();
+            var items = repository.GetItems().Select(item => item.AsDto());
+
             return items;
         }
 
         
         [HttpGet("{id}")] //GET ./item/id
-        public ActionResult<Item> GetItem(Guid id)
+        public ActionResult<ItemDto> GetItem(Guid id)
         {
             var item = repository.GetItem(id);
             if (item is null) return NotFound();
-            return item;
+            
+            return item.AsDto();
         }
 
     }
--- catalog/Repositories/InMemItemsRepository.cs
+++ catalog/Repositories/InMemItemsRepository.cs
@@ -5,7 +5,8 @@ using Catalog.Entities;
 
 namespace Catalog.Repositories
 {
-    public class InMemItemsRepository
+
+    public class InMemItemsRepository : IItemsRepository
     {
         private readonly List<Item> items = new()
         {
